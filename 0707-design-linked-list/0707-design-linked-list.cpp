b7b128217943578074877ba33500794d1c2b4497
Pratik-Singh-01 <162071413+Pratik-Singh-01@users.noreply.github.com>
2026-03-26 16:52:24 +0530

    Time: 7 ms (65.1%), Space: 26 MB (34.93%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.cpp
@@ -0,0 +1,88 @@
+class MyLinkedList {
+public:
+    class Node{
+        public:
+        int val;
+        Node *next;
+
+        Node(int data)
+        {
+            val = data;
+            next = nullptr;
+        }
+    };
+
+    Node *dummy;
+    int size;
+
+    MyLinkedList() {
+        dummy = new Node(0);
+        size =0;
+    }
+    
+    int get(int index) {
+        if(index<0 || index >= size) return -1;
+
+        Node *temp = dummy->next;
+        for(int i=0;i<index;i++)
+        {
+            temp = temp->next;
+        }
+        return temp->val;
+    }
+    
+    void addAtHead(int val) {   
+        addAtIndex(0,val);
+    }
+    
+    void addAtTail(int val) {
+        addAtIndex(size,val);
+    }
+    
+    void addAtIndex(int index, int val) {
+
+        Node *curr = dummy;
+
+        if(index<0 || index > size) return;
+
+        for(int i=0;i<index;i++)
+        {
+            curr = curr->next;
+        }
+
+        Node *temp =new Node(val);
+
+        temp->next = curr->next;
+        curr->next = temp;
+
+        size++;
+    }
+    
+    void deleteAtIndex(int index) {
+        
+        Node *temp = dummy;
+
+        if(index<0 || index >= size) return;
+
+        for(int i=0;i<index;i++)
+        {
+            temp = temp->next;
+        }
+
+        Node *todelete = temp->next;;
+        temp->next = todelete->next;
+
+        delete todelete;
+        size--;
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
